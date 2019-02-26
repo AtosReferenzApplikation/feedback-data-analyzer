72c6a7b43edd2a245c0ca40f0ca2374615a3e0ab
Kleemann <lea-elina.kleemann@atos.net>
2019-02-26 15:42:35 +0100

    WordCloud added

--- ver2.py
+++ ver2.py
@@ -6,6 +6,8 @@ from pyspark.ml.feature import StopWordsRemover
 from pyspark.ml.feature import CountVectorizer
 from pyspark.ml.feature import IDF
 from pyspark.ml.clustering import LDA
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 data=spark.read.text(r"C:\Users\A704081\Desktop\Daten")
 
@@ -97,3 +99,29 @@ for x in range(len(indices)):
 	print("\n")
 	
 data14 = data13.select("filtered2","IDFFeatures","topicDistribution")
+
+text=data13.select("filtered2").take(data13.count())
+helpdict=[]
+freqdict={}
+for x in range(data13.count()):
+	for y in text[x][0]:
+		helpdict.append(y)
+		
+for i in vocablist:
+	freqdict[i]=0
+	
+for i in helpdict:
+	if i in freqdict:
+		freqdict[i]+=1
+
+wordcloud = WordCloud(width = 3000,height = 2000,background_color = 'black').fit_words(freqdict)
+	
+fig = plt.figure(
+    figsize = (40, 30),\
+    facecolor = 'k',\
+    edgecolor = 'k')
+	
+plt.imshow(wordcloud, interpolation = 'bilinear')
+plt.axis('off')
+plt.tight_layout(pad=0)
+plt.show()
