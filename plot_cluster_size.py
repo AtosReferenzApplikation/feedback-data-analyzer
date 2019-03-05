7d36aa8691973a73e2938f28e3ebd102ba856b41
Lorenz <miriam.lorenz@atos.net>
2019-03-05 15:21:17 +0100

    Clusteranzahl plotten

---
+++ plot_cluster_size.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+x = pd.Series(dictpred)
+y = pd.Series.sort_values(x)
+z = pd.DataFrame(y)
+
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(20,9))
+
+#specify which column of the dataframe to plot (here 0)
+z.plot(y=0, kind = 'pie', ax = axes[0])
+z.plot(kind = 'bar', ax = axes[1])
+# make aspect equal (such that circle is not eliptic)
+axes[0].set_aspect("equal")
+#place the legend at a decent position
+axes[0].legend(loc=1, bbox_to_anchor= (1,0.2), fontsize=11)
+plt.show()
--- plot_elbow.py
+++ plot_elbow.py
@@ -8,14 +8,14 @@ from mpl_toolkits.mplot3d import Axes3D
 from pyspark import SparkContext
 from pyspark.ml.clustering import KMeans
 
-cost = np.zeros(50)
-for k in range(2,50):
+cost = np.zeros(70)
+for k in range(10,70):
     kmeans = KMeans().setK(k).setSeed(1).setFeaturesCol("features")
     model = kmeans.fit(rescaledData.sample(False,0.1, seed=42))
     cost[k] = model.computeCost(rescaledData)
 
 fig, ax = plt.subplots(1,1, figsize =(8,6))
-ax.plot(range(2,50),cost[2:50])
+ax.plot(range(10,70),cost[10:70])
 ax.set_xlabel('k')
 ax.set_ylabel('cost')
 
