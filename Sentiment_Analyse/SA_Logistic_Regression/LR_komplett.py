82ed76b10c75079c413f6de4b35b4c67c7988ccf
Lorenz <miriam.lorenz@atos.net>
2019-04-09 12:02:15 +0200

    Dateipfad geändert

--- Sentiment_Analyse/SA_Logistic_Regression/LR_komplett.py
+++ Sentiment_Analyse/SA_Logistic_Regression/LR_komplett.py
@@ -122,7 +122,7 @@ ergebnis = 5
 
 ####################################### Anwendung
 
-df = spark.read.text(r"C:\Users\A704194\projects\feedback-data-analyzer\TR\Projekt\test\pos\0_10.txt")
+df = spark.read.text(r"C:\Users\A704194\projects\feedback-data-analyzer\Daten\TE_Englisch\pos\0_10.txt")
 
 regtok = regexTokenizer.transform(df)
 filrow = remover1.transform(regtok).select("filtered").take(regtok.count())
